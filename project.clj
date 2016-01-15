be9dd250829356bb49a2c2d4a25c9e71aff2667c
Christian Meichsner <christian.meichsner@live.com>
2016-01-15 10:32:09 +0100

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
 (defproject org.lambdaroyal/clojure-algorithms "0.1-SNAPSHOT"
   :description "LambdaRoyal Clojure Algorithms"
   :url "https://github.com/gixxi/clojure-algorithms"
-  :license {:name "LGPL v3"
-            :url "http://www.gnu.org/licenses/lgpl.html"}
+  :license {:name "FreeBSD License"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.nrepl "0.2.3"]
                  [org.lambdaroyal/clojure-util "1.0-SNAPSHOT"]
